85b7bade9a3faff0747872c16a603909f9500e2a
Tomas Ortiz <TomasOrtiz@protonmail.com>
2019-12-03 12:48:47 -0800

    added first event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+
+// Heres 1
+const fatBus = document.querySelector(".intro");
+console.log(fatBus);
+
+fatBus.addEventListener("mouseenter", () => {
+  fatBus.style.transform = "scale(1.2)";
+  fatBus.style.transition = "transform 0.2s";
+});
+fatBus.addEventListener("mouseleave", () => {
+  fatBus.style.transform = "scale(1)";
+});
+
+// Heres 2
+
+// Heres 3
+// Heres 4
+// Heres 5
+// Heres 6
+// Heres 7
+// Heres 8
+// Heres 9
+// Heres 10
