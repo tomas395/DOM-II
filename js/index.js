86395669703aac8adb6d8babc852cb097f5067fa
Tomas Ortiz <TomasOrtiz@protonmail.com>
2019-12-03 15:48:49 -0800

    this is unfinished but submitting anyway

--- js/index.js
+++ js/index.js
@@ -2,23 +2,67 @@
 
 // Heres 1
 const fatBus = document.querySelector(".intro");
-console.log(fatBus);
 
 fatBus.addEventListener("mouseenter", () => {
   fatBus.style.transform = "scale(1.2)";
   fatBus.style.transition = "transform 0.2s";
 });
+// Heres 2
 fatBus.addEventListener("mouseleave", () => {
   fatBus.style.transform = "scale(1)";
 });
 
-// Heres 2
-
 // Heres 3
+
+const stopProp = document.querySelector("body");
+stopProp.addEventListener("click", event => {
+  stopProp.style.backgroundColor = "dodgerblue";
+  event.stopPropagation();
+});
+
 // Heres 4
+const navClrClk = document.querySelector("header");
+navClrClk.addEventListener("click", () => {
+  navClrClk.style.backgroundColor = "gold";
+});
+
 // Heres 5
+window.addEventListener("load", () => {
+  alert(
+    "Hi im just an annoying onload message! Forget this ever happened and enjoy the rest of the site."
+  );
+});
+
 // Heres 6
+const footerB = document.querySelector("footer");
+footerB.addEventListener("wheel", () => {
+  footerB.style.backgroundColor = "silver";
+});
+
 // Heres 7
+const stopLink = document.querySelector("nav");
+stopLink.addEventListener("click", el => {
+  el.preventDefault();
+  console.log("haha");
+});
+
 // Heres 8
+const navLinks = document.querySelectorAll(".nav-link");
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    link.style.color = "red";
+  });
+});
+
 // Heres 9
+const scrollEvent = document.querySelector("body");
+window.addEventListener("scroll", () => {
+  body.style.backgroundColor = "red";
+});
+
 // Heres 10
+
+const flipThisH2 = document.querySelector(".intro p");
+flipThisH2.addEventListener("click", event => {
+  flipThisH2.style.transform = "rotate(-90deg)";
+});
